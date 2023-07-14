6961f48bbbaf7064e711db4dfdd1e30678b6d62d
Jean <jeanhaley32@gmail.com>
2023-07-14 17:19:54 -0400

    Cleaned up test, fixed comments with incorrect information.

--- twitch-auth.go
+++ twitch-auth.go
@@ -27,7 +27,6 @@ type token struct {
 	AccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
 	ExpiresIn   int64  `json:"expires_in"`
-	Scope       string `json:"scope"`
 }
 
 type TwitchAuthInterface interface {
@@ -35,7 +34,7 @@ type TwitchAuthInterface interface {
 }
 
 // Obtains a new Token set from the Twitch API
-// Token set includes access toke, and refresh token
+// Token set includes access token, Type, expiration time
 func (self TwitchAuth) NewTokenSet() error {
 	var t token
 	// Client credentials grant flow
@@ -50,25 +49,10 @@ func (self TwitchAuth) NewTokenSet() error {
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	// // (test) Print the whole request to the console for debugging
-	// dump, err := httputil.DumpRequestOut(req, true)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// fmt.Printf("\nRequest: %s\n", dump)
-
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
-
-	// // (test) Print Server Response
-	// dump, err = httputil.DumpResponse(resp, true)
-	// if err != nil {
-	// 	return err
-	// }
-	// fmt.Printf("\nResponse: %s\n", dump)
 
 	defer resp.Body.Close()
 
